9026114bbd8785508b46a060d34f12a32c4cdf49
Robert Hensing <robert.hensing@inology.nl>
2015-09-15 16:41:09 +0200

    KISTOT-916: Move algorithm to its own object without maven dependencies

---
+++ src/main/scala/nl/ebpi/mavenplugins/ebpienforcer/DependencyConvergence.scala
@@ -0,0 +1,96 @@
+package nl.ebpi.mavenplugins.ebpienforcer
+
+/**
+ * key needed to find projects while searching for conflicting versions. Projects with this key
+ * must have the same version, or it will be an error
+ *
+ * @author adesole
+ */
+case class ProjectKey(groupId: String, artifactId: String)
+
+/**
+ * groupId, artifactId, version
+ */
+case class Project(key: ProjectKey, version: String)
+
+case class DependentChain(involvedProjects: List[Project]) {
+  def tail: DependentChain = DependentChain(involvedProjects.tail)
+  def head: Project = involvedProjects.head
+
+  /**
+   * Transforms the dependent chain into a dependency chain.
+   *
+   * reverse.head depends on the reverse.tail
+   */
+  def reverse: List[Project] = involvedProjects.reverse
+}
+
+/**
+ * See [[DependencyConvergenceGoal]]
+ */
+object DependencyConvergence {
+
+  type Conflict = Map[Project, List[DependentChain]]
+
+
+  def determineConflicts(allDependentChains: List[DependentChain], isAssumedSufficientFor: Map[Project, Project]): Iterable[Conflict] = {
+
+    def toBeReplacedBy(someProject: Project): Project = {
+      isAssumedSufficientFor.getOrElse(someProject, someProject)
+    }
+
+    /**
+     * Partitions a list of chains by the project *key* that the chain starts from.
+     * This groups together possible conflicts, because the version is not included in the partitioning key.
+     */
+    def byKey(chains: List[DependentChain]): Map[ProjectKey, List[DependentChain]] = {
+      chains.groupBy(_.involvedProjects.head.key)
+    }
+
+
+    def goDetermineConflicts: Iterable[Conflict] = {
+      val allChains = allDependentChains
+      val groupedProjectsWithDependents = byKey(allChains).values
+      groupedProjectsWithDependents.map(conflictOption).flatMap(Option.option2Iterable)
+    }
+
+    /**
+     * Checks if the list of chains starts with the same project (version).
+     * Returns a map of sides.
+     *
+     * @param chains chains such that for any elements p and q in chains, p.key == q.key
+     * @return possible conflict
+     */
+    def conflictOption(chains: List[DependentChain]): Option[Conflict] = {
+      val sides = chains.groupBy(_.involvedProjects.head)
+
+      val sidesAfterAssumptions = (sides.keys map toBeReplacedBy).toSet
+
+      if (sidesAfterAssumptions.size > 1)
+        Some(sides)
+      else
+        None
+    }
+
+    def shrinkConflicts(allConflicts: Iterable[Conflict]): Iterable[Conflict] = {
+
+      // Remove one layer of failed dependencies to see if a smaller subproblem
+      // can be presented to the user without loss of information. (Problems near the root must be resolved first)
+      def stripConflict: Conflict => Option[Conflict] = { conflict =>
+        val strippedChains = conflict.values.flatten.map(_.tail).toList
+        conflictOption(strippedChains)
+      }
+
+      val allConflictsSet = allConflicts.toSet
+      allConflicts filter (stripConflict andThen {
+        case None => true // no simpler further conflict, so we need to show this one
+        case Some(strippedConflict) => // forget about this one if a simpler conflict is already included
+          !allConflictsSet.contains(strippedConflict)
+      })
+    }
+
+
+    shrinkConflicts(goDetermineConflicts)
+  }
+
+}
--- src/main/scala/nl/ebpi/mavenplugins/ebpienforcer/DependencyConvergenceGoal.scala
+++ src/main/scala/nl/ebpi/mavenplugins/ebpienforcer/DependencyConvergenceGoal.scala
@@ -2,6 +2,7 @@ package nl.ebpi.mavenplugins.ebpienforcer
 
 import java.util
 
+import nl.ebpi.mavenplugins.ebpienforcer.DependencyConvergence.Conflict
 import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager
 import org.apache.maven.artifact.repository.ArtifactRepository
 import org.apache.maven.artifact.resolver.ArtifactResolver
@@ -102,13 +103,15 @@ class DependencyConvergenceGoal extends AbstractMojo {
   protected var assumptions : java.util.List[Assumption] = new util.ArrayList[Assumption]()
 
   override def execute(): Unit = {
-    val conflicts = determineConflicts
+    //val conflicts = determineConflicts
+    val conflicts = DependencyConvergence.determineConflicts(
+      allDependentChains = dependentChainsForAllDependencies(project).map(DependentChain(_)),
+      isAssumedSufficientFor = currentAssumptions)
 
     if (conflicts.nonEmpty) {
       // Remove conflicts that follow from other conflicts before presenting them.
       // This happens after _.nonEmpty, because it is more defensive.
-      val shrunk = shrinkConflicts(conflicts)
-      displayConflictsAndFail(shrunk)
+      displayConflictsAndFail(conflicts)
     }
   }
 
@@ -139,7 +142,7 @@ class DependencyConvergenceGoal extends AbstractMojo {
     throw new MojoFailureException("Conflicting versions found. See diagnostics above.")
   }
 
-  private def renderTree(dependents: List[List[Project]]): List[String] = {
+  private def renderTree(dependents: List[DependentChain]): List[String] = {
     // convert from dependent chain to dependency chain
     val dependencies = dependents.map(_.reverse)
 
@@ -154,30 +157,6 @@ class DependencyConvergenceGoal extends AbstractMojo {
       ls.toList
     }
     lines
-  }
-
-  private def determineConflicts: Iterable[Conflict] = {
-    // Is there no scala library support for partial function { case Some(x) => x }  ?
-    val allChains = dependentChainsForAllDependencies(project)
-    val groupedProjectsWithDependents = byKey(allChains).values
-    groupedProjectsWithDependents.map(conflictOption).flatMap(Option.option2Iterable)
-  }
-
-  private def shrinkConflicts(allConflicts : Iterable[Conflict]) : Iterable[Conflict] = {
-
-    // Remove one layer of failed dependencies to see if a smaller subproblem
-    // can be presented to the user without loss of information. (Problems near the root must be resolved first)
-    def stripConflict : Conflict => Option[Conflict] = { conflict =>
-        val strippedChains = conflict.values.flatten.map(_.tail).toList
-        conflictOption(strippedChains)
-    }
-
-    val allConflictsSet = allConflicts.toSet
-    allConflicts filter (stripConflict andThen {
-      case None => true // no simpler further conflict, so we need to show this one
-      case Some(strippedConflict) => // forget about this one if a simpler conflict is already included
-        !allConflictsSet.contains(strippedConflict)
-    })
   }
 
   /**
@@ -197,34 +176,6 @@ class DependencyConvergenceGoal extends AbstractMojo {
     }
 
     withAncestors(project, toProject(project) :: Nil, dependency => true)
-  }
-
-  /**
-   * Partitions a list of chains by the project *key* that the chain starts from.
-   * This groups together possible conflicts, because the version is not included in the partitioning key.
-   */
-  private def byKey(chains : List[List[Project]]): Map[ProjectKey, List[List[Project]]] = {
-    chains.groupBy(_.head.key).toMap
-  }
-
-  type Conflict = Map[Project, List[List[Project]]]
-
-  /**
-   * Checks if the list of chains starts with the same project (version).
-   * Returns a map of sides.
-   *
-   * @param chains chains such that for any elements p and q in chains, p.key == q.key
-   * @return possible conflict
-   */
-  private def conflictOption(chains : List[List[Project]]) : Option[Conflict] = {
-    val sides = chains.groupBy(_.head)
-
-    val sidesAfterAssumptions = (sides.keys map toBeReplacedBy).toSet
-
-    if (sidesAfterAssumptions.size > 1)
-      Some(sides)
-    else
-      None
   }
 
   private def findDuplicates[A,B](iterable : List[(A, B)]) : Map[A, List[(A, B)]] = {
@@ -249,17 +200,13 @@ class DependencyConvergenceGoal extends AbstractMojo {
       dependencies.toMap
   }
 
-  lazy val currentAssumptions : Map[Project, Project] = {
+  def currentAssumptions : Map[Project, Project] = {
     (for {
       assumption <- assumptions.iterator().asScala
       replacement = toProject(assumption.artifact)
       isSufficientFor <- assumption.isSufficientFor.split(",").map(_.trim)
       replaceable = Project(replacement.key, isSufficientFor)
     } yield (replaceable, replacement)).toMap
-  }
-
-  private def toBeReplacedBy(someProject : Project) : Project = {
-    currentAssumptions.getOrElse(someProject, someProject)
   }
 
   /**
@@ -304,25 +251,12 @@ class DependencyConvergenceGoal extends AbstractMojo {
 }
 
 object DependencyConvergenceGoal {
-  /**
-   * key needed to find projects while searching for conflicting versions. Projects with this key
-   * must have the same version, or it will be an error
-   *
-   * @author adesole
-   */
-  case class ProjectKey(groupId: String, artifactId: String)
 
   private def toProjectKey(project : MavenProject) : ProjectKey =
     ProjectKey(project.getGroupId, project.getArtifactId)
 
   private def toProjectKey(dependency : Dependency) : ProjectKey =
     ProjectKey(dependency.getGroupId, dependency.getArtifactId)
-
-
-  /**
-   * groupId, artifactId, version
-   */
-  case class Project(key : ProjectKey, version : String)
 
   private def toProject(project : MavenProject) : Project =
     Project(toProjectKey(project), project.getVersion)
